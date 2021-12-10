39886cbb3662ec94a6495c362eb8e95b0de0a8c2
Jimmy P <jimmyporinsu@gmail.com>
2021-12-09 19:09:20 -0500

    Dependencias Apollo src/main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-import { vfmPlugin } from 'vue-final-modal'
-
-createApp(App).use(store).use(router).use(vfmPlugin).mount('#app')
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+import { setContext } from 'apollo-link-context'
+const httpLink = createHttpLink({
+    uri: 'https://gcuatroapigateway.herokuapp.com/',
+})
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            "Authorization": localStorage.getItem("token_access") || ""
+        }
+    }
+})
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+})
+const apolloProvider = new createApolloProvider({
+    defaultClient: apolloClient
+})
+createApp(App).use(router).use(apolloProvider).mount('#app')
